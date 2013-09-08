aa8fd1bb109776c5ada0c9ec05c6ddff0e9cf469
Eduard Bröcker <edu@iwn-eee.(none)>
2013-09-08 20:29:26 +0200

    stringlength of worksheetname limited to maximal 32 characters

--- library/exportxls.py
+++ library/exportxls.py
@@ -212,7 +212,7 @@ def exportXls(db, filename):
 	head_tail = ['Value',	'Name / Phys. Range', 'Function / Increment Unit']
 
 	workbook = xlwt.Workbook(encoding = 'utf8')
-	worksheet = workbook.add_sheet('K-Matrix ' + filename.replace('.xls',''))
+	worksheet = workbook.add_sheet('K-Matrix ' + filename.replace('.xls','')[0:22])
 	col = 0
 
 	# write first row (header) cols before frameardunits:
