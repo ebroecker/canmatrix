4e3dde3b18c2e85161c6d4981a3769b6ba1c35af
Kyle Altendorf <kyle.altendorf@epcpower.com>
2015-11-27 12:09:10 -0500

    Add preliminary test script and reference results
    Ref issue #7

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Copyright (c) 2013, Eduard Broecker 
+#Copyright (c) 2013, Eduard Broecker
 #All rights reserved.
 #
 #Redistribution and use in source and binary forms, with or without modification, are permitted provided that
@@ -24,89 +24,97 @@ import library.importall as im
 import library.canmatrix as cm
 import os
 import sys
-from optparse import OptionParser
-
-usage = """
-%prog [options] import-file export-file
-
-import-file: *.dbc|*.dbf|*.kcd|*.arxml|*.xls(x)|*.sym
-export-file: *.dbc|*.dbf|*.kcd|*.json|*.xls(x)
-
-"""
-
-parser = OptionParser(usage=usage)
-#parser.add_option("-d", "--debug",
-#                  dest="debug", default=False,
-#                  help="print debug messages to stdout")
-parser.add_option("", "--dbcCharset", 
-                  dest="dbcCharset", default="iso-8859-1",
-                  help="Charset of Comments in dbc, maybe utf-8")
-parser.add_option("", "--dbcCommentCharset", 
-                  dest="dbcCommentCharset", default="iso-8859-1",
-                  help="Charset of Comments in dbc")
-(cmdlineOptions, args) = parser.parse_args()
 
 
-if len(args) < 2:
-    parser.print_help()
-    sys.exit(1)
-
-dbs = {}
-infile = args[0]
-outfileName = args[1]
-
-print "Importing " + infile + " ... "
-if infile[-3:] == 'dbc':
-	dbs[""] = im.importDbc(infile, cmdlineOptions.dbcCharset,  cmdlineOptions.dbcCommentCharset)
-elif infile[-3:] == 'dbf':
-	dbs[""] = im.importDbf(infile)
-elif infile[-3:] == 'sym':
-	dbs[""] = im.importSym(infile)
-elif infile[-3:] == 'kcd':
-	dbs[""] = im.importKcd(infile)
-elif infile[-3:] == 'xls':
-	dbs[""] = im.importXls(infile)
-elif infile[-4:] == 'xlsx' :
-	dbs[""] = im.importXlsx(infile)
-elif infile[-5:] == 'arxml':
-	dbs = im.importArxml(infile)
-elif infile[-4:] == 'yaml':
-	dbs[""] = im.importYaml(infile)
-else:
-    sys.stderr.write('\nFile not recognized: ' + infile + "\n")
-print "done\n"
-
-
-print "Exporting " + outfileName + " ... "
-
-for name in dbs:
-	db = dbs[name]
-	print name
-	print "%d Frames found" % (db._fl._list.__len__())
-	
-	if len(name) > 0:
-		path = os.path.split(outfileName)
-		outfile = os.path.join(path[0], name + "_" + path[1])
+def convert(infile, outfileName, dbcCharset='iso-8859-1', dbcCommentCharset='iso-8859-1'):
+	dbs = {}
+	print "Importing " + infile + " ... "
+	if infile[-3:] == 'dbc':
+		dbs[""] = im.importDbc(infile, dbcCharset,  dbcCommentCharset)
+	elif infile[-3:] == 'dbf':
+		dbs[""] = im.importDbf(infile)
+	elif infile[-3:] == 'sym':
+		dbs[""] = im.importSym(infile)
+	elif infile[-3:] == 'kcd':
+		dbs[""] = im.importKcd(infile)
+	elif infile[-3:] == 'xls':
+		dbs[""] = im.importXls(infile)
+	elif infile[-4:] == 'xlsx' :
+		dbs[""] = im.importXlsx(infile)
+	elif infile[-5:] == 'arxml':
+		dbs = im.importArxml(infile)
+	elif infile[-4:] == 'yaml':
+		dbs[""] = im.importYaml(infile)
 	else:
-		outfile = outfileName
-	if outfile[-3:] == 'dbc':
-		ex.exportDbc(db, outfile, cmdlineOptions.dbcCharset,  cmdlineOptions.dbcCommentCharset)
-	elif outfile[-3:] == 'dbf':
-		ex.exportDbf(db, outfile)
-	elif outfile[-3:] == 'sym':
-		ex.exportSym(db, outfile)
-	elif outfile[-3:] == 'kcd':
-		ex.exportKcd(db, outfile)
-	elif outfile[-4:] == 'xlsx':
-		ex.exportXlsx(db, outfile)
-	elif outfile[-3:] == 'xls':
-		ex.exportXls(db, outfile)
-	elif outfile[-4:] == 'json':
-		ex.exportJson(db, outfile)
-	elif outfile[-5:] == 'arxml':
-		ex.exportArxml(db, outfile)
-	elif outfile[-4:] == 'yaml':
-		ex.exportYaml(db, outfile)
-	else:
-	    sys.stderr.write('File not recognized: ' + outfile + "\n")
-print "done"
+		sys.stderr.write('\nFile not recognized: ' + infile + "\n")
+	print "done\n"
+
+
+	print "Exporting " + outfileName + " ... "
+
+	for name in dbs:
+		db = dbs[name]
+		print name
+		print "%d Frames found" % (db._fl._list.__len__())
+
+		if len(name) > 0:
+			path = os.path.split(outfileName)
+			outfile = os.path.join(path[0], name + "_" + path[1])
+		else:
+			outfile = outfileName
+		if outfile[-3:] == 'dbc':
+			ex.exportDbc(db, outfile, dbcCharset,  dbcCommentCharset)
+		elif outfile[-3:] == 'dbf':
+			ex.exportDbf(db, outfile)
+		elif outfile[-3:] == 'sym':
+			ex.exportSym(db, outfile)
+		elif outfile[-3:] == 'kcd':
+			ex.exportKcd(db, outfile)
+		elif outfile[-4:] == 'xlsx':
+			ex.exportXlsx(db, outfile)
+		elif outfile[-3:] == 'xls':
+			ex.exportXls(db, outfile)
+		elif outfile[-4:] == 'json':
+			ex.exportJson(db, outfile)
+		elif outfile[-5:] == 'arxml':
+			ex.exportArxml(db, outfile)
+		elif outfile[-4:] == 'yaml':
+			ex.exportYaml(db, outfile)
+		else:
+			sys.stderr.write('File not recognized: ' + infile + "\n")
+	print "done"
+
+def main():
+	from optparse import OptionParser
+
+	usage = """
+	%prog [options] import-file export-file
+
+	import-file: *.dbc|*.dbf|*.kcd|*.arxml|*.xls(x)|*.sym
+	export-file: *.dbc|*.dbf|*.kcd|*.json|*.xls(x)
+
+	"""
+
+	parser = OptionParser(usage=usage)
+	#parser.add_option("-d", "--debug",
+	#                  dest="debug", default=False,
+	#                  help="print debug messages to stdout")
+	parser.add_option("", "--dbcCharset",
+					  dest="dbcCharset", default="iso-8859-1",
+					  help="Charset of Comments in dbc, maybe utf-8")
+	parser.add_option("", "--dbcCommentCharset",
+					  dest="dbcCommentCharset", default="iso-8859-1",
+					  help="Charset of Comments in dbc")
+	(cmdlineOptions, args) = parser.parse_args()
+
+	if len(args) < 2:
+		parser.print_help()
+		sys.exit(1)
+	infile = args[0]
+	outfileName = args[1]
+
+	convert(infile=infile, outfile=outfileName,
+			charset=args.dbcCharset, comment_charset=args.commentCharset)
+
+if __name__ == '__main__':
+	sys.exit(main())
---
+++ test/test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import sys
+sys.path.append('..')
+
+import convert
+import copy
+import os
+import re
+import shutil
+import subprocess
+
+
+export_types = []
+import_types = []
+for f in os.listdir('../library'):
+    m = re.match('^export(.*).py$', f)
+    if m is not None and m.group(1) != 'all':
+        export_types.append(m.group(1))
+    m = re.match('^import(.*).py$', f)
+    if m is not None and m.group(1) != 'all' and m.group(1) != 'any':
+        import_types.append(m.group(1))
+
+export_types.sort()
+# TODO: support testing of xlsx
+export_types.remove('xlsx')
+import_types.sort()
+
+test_file_base = 'test'
+converted_path = 'converted'
+try:
+    shutil.rmtree(converted_path)
+except OSError:
+    # it's already not there...
+    pass
+
+for i in import_types:
+    in_file = test_file_base + '.' + i.lower()
+    if not os.path.isfile(in_file):
+        print('Skipping conversion from missing file ' + in_file)
+    else:
+        to = copy.copy(export_types)
+        try:
+            to.remove(i)
+        except ValueError:
+            # TODO: support testing of xlsx
+            pass
+        print('{} -> {}'.format(i, to))
+
+        for t in to:
+            out_file = os.path.basename(test_file_base)
+            # out_file = os.path.splitext(out_file)[0]
+            out_file += '.' + t.lower()
+            directory = os.path.join(converted_path, 'from_' + i)
+            try:
+                os.makedirs(directory)
+            except OSError:
+                # TODO: be more specific: OSError: [Errno 17] File exists: 'converted/from_arxml'
+                pass
+            out_file = os.path.join(directory, out_file)
+            if t == 'dbf':
+                # TODO: https://github.com/ebroecker/canmatrix/issues/9
+                continue
+            convert.convert(in_file, out_file)
+
+exit_code = subprocess.call(['diff', '-r', 'reference', 'converted'])
+
+if exit_code:
+    # difference found
+    message = 'difference found'
+else:
+    # no difference found
+    message = 'no difference'
+
+print('\n\n    Testing completed: {message}'.format(**locals()))
