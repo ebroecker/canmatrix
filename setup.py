397d35d9e34b64a8e9f49c16fe4b68e211162b46
eduard <eduard@gmx.de>
2016-01-14 22:36:00 +0100

    release 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "canmatrix",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages(),
     entry_points={'console_scripts': ['cancompare = canmatrix.compare:main',
                                       'canconvert = canmatrix.convert:main']}
