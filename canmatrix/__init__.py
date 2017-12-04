bf135f59f10b56389e354ead4b68fbaeab7546d8
ebroecker <eduard@gmx.de>
2017-12-04 22:45:48 +0100

    make test runable again

--- canmatrix/__init__.py
+++ canmatrix/__init__.py
@@ -1,5 +1,8 @@
-import formats
-import cancluster
-import convert
-import copy
+#!/usr/bin/env python
+from __future__ import absolute_import
+
+import canmatrix.formats as formats
+import canmatrix.cancluster as cancluster
+import canmatrix.convert as convert
+import canmatrix.copy as copy
 from canmatrix import *
