18e881c3dd0b0079da44f048e5e53dabd9206820
ebroecker <eduard@gmx.de>
2017-12-29 20:15:59 +0100

    fix python3 working again

--- canmatrix/sym.py
+++ canmatrix/sym.py
@@ -260,11 +260,14 @@ Title=\"canmatrix-Export\"
 
 
 def mySplit(inLine):
-    tempArray = shlex.split(inLine.strip().encode('utf-8'))
-    newArray = []
-    for item in tempArray:
-        newArray.append(item.decode('utf-8'))
-    return newArray
+    if sys.version_info > (3, 0):  # is there a clean way to to it?
+        return shlex.split(inLine.strip())
+    else:
+        tempArray = shlex.split(inLine.strip().encode('utf-8'))
+        newArray = []
+        for item in tempArray:
+            newArray.append(item.decode('utf-8'))
+        return newArray
 
 
 def load(f, **options):
