eef25e051c7ce650640ddecf43eaff00154d875a
Tomas Novak <ext.Tomas.Novak@skoda-auto.cz>
2018-12-04 17:03:24 +0100

    fix docstring quotes

--- src/canmatrix/canmatrix.py
+++ src/canmatrix/canmatrix.py
@@ -490,7 +490,7 @@ class DecodedSignal(object):
 
 # https://docs.python.org/3/library/itertools.html
 def grouper(iterable, n, fillvalue=None):
-    """"Collect data into fixed-length chunks or blocks"""
+    """Collect data into fixed-length chunks or blocks."""
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
