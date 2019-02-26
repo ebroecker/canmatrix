969b8435fa049a6461c2ee7a7d71738408c1d080
Tomas Novak <ext.Tomas.Novak@skoda-auto.cz>
2019-02-26 18:21:18 +0100

    Resolve inspect issues in tests module (pep8)

--- src/canmatrix/tests/test_arxml.py
+++ src/canmatrix/tests/test_arxml.py
@@ -2,12 +2,12 @@
 import canmatrix.arxml
 import pathlib2
 
+
 def test_ecu_extract():
     here = pathlib2.Path(__file__).parent
     
     db = canmatrix.arxml.load(str(here / "MyECU.ecuc.arxml"))['']
-    assert db.frames != None
+    assert db.frames is not None
     assert len(db.frames) == 2
     assert len(db.frames[0].signals) == 3
     assert len(db.frames[1].signals) == 1
-        
