61c47b9a7efc4e13384ba67908ae8d93e0e2311e
Funth0mas <43621609+Funth0mas@users.noreply.github.com>
2019-03-22 20:02:54 +0100

    Clean up csv (#328)

--- src/canmatrix/formats/cmcsv.py
+++ src/canmatrix/formats/cmcsv.py
@@ -5,9 +5,9 @@
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 # the following conditions are met:
 #
-#    Redistributions of source code must retain the aframeve copyright notice, this list of conditions and the
+#    Redistributions of source code must retain the above copyright notice, this list of conditions and the
 #    following disclaimer.
-#    Redistributions in binary form must reproduce the aframeve copyright notice, this list of conditions and the
+#    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
 #    following disclaimer in the documentation and/or other materials provided with the distribution.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
@@ -20,7 +20,7 @@
 # DAMAGE.
 
 #
-# this script exports canmatrix-objects to a CSV file. (Based on exportxlsx)
+# this script exports canmatrix-objects to a CSV file. (Based on xlsx)
 # Author: Martin Hoffmann (m8ddin@gmail.com)
 
 from __future__ import absolute_import
@@ -34,68 +34,71 @@ import typing
 import canmatrix.formats.xls_common
 
 logger = logging.getLogger(__name__)
+CsvDataType = typing.Union[str, int]
 
 extension = 'csv'
 
 
 class CsvRow:
-    def __init__(self):
-        self._rowdict = collections.defaultdict(str)  # type: typing.Dict[str, str]
+    def __init__(self):  # type: () -> None
+        self._row_dict = collections.defaultdict(str)  # type: typing.Dict[int, CsvDataType]
 
-    def __getitem__(self, key):
-        return self._rowdict[key]
+    def __getitem__(self, key):  # type: (int) -> CsvDataType
+        return self._row_dict[key]
 
-    def __setitem__(self, key, item):
-        if (sys.version_info <= (3, 0)):
+    def __setitem__(self, key, item):  # type: (int, CsvDataType) -> None
+        if sys.version_info <= (3, 0):
             if type(item).__name__ == "unicode":
                 item = item.encode('utf-8')
-        self._rowdict[key] = item
+        self._row_dict[key] = item
 
-    def __add__(self, other):
-        if len(self._rowdict.keys()) > 0:
-            start = max(self._rowdict.keys()) +1
+    def __add__(self, other):  # type: (typing.Iterable[CsvDataType]) -> CsvRow
+        if len(self._row_dict.keys()) > 0:
+            start = max(self._row_dict.keys()) + 1
         else:
             start = 0
         i = 0
         for item in other:
-            self[start+i] = item
+            self[start + i] = item
             i += 1
         return self
 
-    def write(self, column, value):
-        self._rowdict[column] = value
+    def write(self, column, value):  # type: (int, CsvDataType) -> None
+        self._row_dict[column] = value
 
     @property
-    def as_list(self):
+    def as_list(self):  # type: () -> typing.List[str]
         # Generate list of single cells in the row till highest index (dictionary key)
         # Empty cells (non-existent keys) are generated as empty string
-        return [str(self._rowdict[x])
-                for x in range(0, max(self._rowdict) + 1)]
+        return [str(self._row_dict[x])
+                for x in range(0, max(self._row_dict) + 1)]
 
-    def toCSV(self, delimiter=','):
+    def to_csv(self, delimiter=','):  # type: (str) -> str
         text = delimiter.join(self.as_list)
         return text.replace('\n', ' ')
 
-    def __str__(self):
-        return self.toCSV()
+    def __str__(self):  # type: () -> str
+        return self.to_csv()
 
-def writeBuMatrixx(buList, sig, frame, row, col):
-    # iterate over boardunits:
-    for bu in buList:
-        # write "s" "r" "r/s" if signal is sent, received or send and received
-        # by boardunit
-        if bu in sig.receivers and bu in frame.transmitters:
+
+def write_ecu_matrix(ecu_name_list, sig, frame, row, col):
+    # type: (typing.Sequence[str], canmatrix.Signal, canmatrix.Frame, CsvRow, int) -> int
+    # iterate over ecus:
+    for ecu_name in ecu_name_list:
+        # write "s" "r" "r/s" if signal is sent, received or send and received by ECU
+        if ecu_name in sig.receivers and ecu_name in frame.transmitters:
             row[col] = "r/s"
-        elif bu in sig.receivers:
+        elif ecu_name in sig.receivers:
             row[col] = "r"
-        elif bu in frame.transmitters:
+        elif ecu_name in frame.transmitters:
             row[col] = "s"
         else:
             pass
         col += 1
     return col
 
-def dump(db, thefile, delimiter=',', **options):
+
+def dump(db, file_object, delimiter=',', **options):
     # type: (canmatrix.CanMatrix, typing.BinaryIO, str, **str) -> None
     head_top = [
         'ID',
@@ -112,157 +115,145 @@ def dump(db, thefile, delimiter=',', **options):
         ' Signal Not Available',
         'Byteorder',
         'is signed']
-    head_tail = ['Name / Phys. Range', 'Function / Increment Unit','Value']
+    head_tail = ['Name / Phys. Range', 'Function / Increment Unit', 'Value']
 
-    if "additionalAttributes" in options and len(options["additionalAttributes"]) > 0:
-        additionalSignalCollums = options["additionalAttributes"].split(",")
-    else:
-        additionalSignalCollums = []#["attributes['DisplayDecimalPlaces']"]
+    additional_signal_columns = options.get("additionalAttributes", "").split(",")
+    additional_frame_columns = options.get("additionalFrameAttributes", "").split(",")
+    motorola_bit_format = options.get("xlsMotorolaBitFormat", "msbreverse")
 
-    if "additionalFrameAttributes" in options and len(options["additionalFrameAttributes"]) > 0:
-        additionalFrameCollums = options["additionalFrameAttributes"].split(",")
-    else:
-        additionalFrameCollums = []#["attributes['DisplayDecimalPlaces']"]
-
-    if 'xlsMotorolaBitFormat' in options:
-        motorolaBitFormat = options["xlsMotorolaBitFormat"]
-    else:
-        motorolaBitFormat = "msbreverse"
-
-    csvtable = list()  # List holding all csv rows
+    csv_table = list()  # List holding all csv rows
 
     col = 0  # Column counter
 
     # -- headers start:
-    headerrow = CsvRow()
+    header_row = CsvRow()
 
-    # write first row (header) cols before frameardunits:
+    # write first row (header) cols before ECUs:
     for head in head_top:
-        headerrow.write(col, head)
+        header_row.write(col, head)
         col += 1
 
-    # write frameardunits in first row:
-    buList = []
-    for bu in db.ecus:
-        headerrow.write(col, bu.name)
-        buList.append(bu.name)
+    # write ECU in first row:
+    ecu_name_list = []
+    for ecu in db.ecus:
+        header_row.write(col, ecu.name)
+        ecu_name_list.append(ecu.name)
         col += 1
 
-    # write first row (header) cols after frameardunits:
+    # write first row (header) cols after ECUs:
     for head in head_tail:
-        headerrow.write(col, head)
+        header_row.write(col, head)
         col += 1
 
-    for additionalCol in additionalFrameCollums:
-        headerrow.write(col,"frame." + additionalCol)
+    for additionalCol in additional_frame_columns:
+        header_row.write(col, "frame." + additionalCol)
         col += 1
 
-    for additionalCol in additionalSignalCollums:
-        headerrow.write(col,"signal." + additionalCol)
+    for additionalCol in additional_signal_columns:
+        header_row.write(col, "signal." + additionalCol)
         col += 1
 
-    csvtable.append(headerrow)
+    csv_table.append(header_row)
     # -- headers end...
 
-    frameHash = {}
+    frame_hash = {}
     for frame in db.frames:
         if frame.is_complex_multiplexed:
             logger.error("export complex multiplexers is not supported - ignoring frame " + frame.name)
             continue
-        frameHash[int(frame.arbitration_id.id)] = frame
+        frame_hash[int(frame.arbitration_id.id)] = frame
 
     # set row to first Frame (row = 0 is header)
     row = 1
 
     # iterate over the frames
-    for idx in sorted(frameHash.keys()):
-        frame = frameHash[idx]
+    for idx in sorted(frame_hash.keys()):
+        frame = frame_hash[idx]
 
         # sort signals:
-        sigHash = {}
+        signal_hash = {}
         for sig in frame.signals:
-            sigHash["%02d" % int(sig.get_startbit()) + sig.name] = sig
+            signal_hash["%02d" % int(sig.get_startbit()) + sig.name] = sig
 
-        additionalFrameInfo = []
-        for frameInfo in additionalFrameCollums:
-            temp = frame.attribute(frameInfo, default="")
-            additionalFrameInfo.append(temp)
-
+        additional_frame_info = [
+            frame.attribute(frameInfo, default="")
+            for frameInfo in additional_frame_columns
+        ]
 
         # iterate over signals
-        for sig_idx in sorted(sigHash.keys()):
-            sig = sigHash[sig_idx]
+        for sig_idx in sorted(signal_hash.keys()):
+            sig = signal_hash[sig_idx]
 
             # value table available?
             if sig.values.__len__() > 0:
-                # iterate over values in valuetable
+                # iterate over values in value table
                 for val in sorted(sig.values.keys()):
-                    signalRow = CsvRow()
-                    signalRow += canmatrix.formats.xls_common.get_frame_info(db, frame)
+                    signal_row = CsvRow()
+                    signal_row += canmatrix.formats.xls_common.get_frame_info(db, frame)
 
-                    (front, back) = canmatrix.formats.xls_common.get_signal(db, sig, motorolaBitFormat)
-                    signalRow += front
-                    signalRow += ("s" if sig.is_signed else "u")
+                    (front, back) = canmatrix.formats.xls_common.get_signal(db, sig, motorola_bit_format)
+                    signal_row += front
+                    signal_row += ("s" if sig.is_signed else "u")
 
                     col = head_top.__len__()
-                    col = writeBuMatrixx(buList, sig, frame, signalRow, col)
-                    signalRow += back
+                    write_ecu_matrix(ecu_name_list, sig, frame, signal_row, col)
+                    signal_row += back
                     # write Value
-                    signalRow += [val, sig.values[val]]
+                    signal_row += [val, sig.values[val]]
 
-                    signalRow += additionalFrameInfo
-                    for item in additionalSignalCollums:
+                    signal_row += additional_frame_info
+                    for item in additional_signal_columns:
                         temp = getattr(sig, item, "")
-                        signalRow += [temp]
+                        signal_row += [temp]
 
                     # next row
                     row += 1
-                    csvtable.append(signalRow)
+                    csv_table.append(signal_row)
                 # loop over values ends here
             # no value table available
             else:
-                signalRow = CsvRow()
-                signalRow += canmatrix.formats.xls_common.get_frame_info(db, frame)
+                signal_row = CsvRow()
+                signal_row += canmatrix.formats.xls_common.get_frame_info(db, frame)
 
-                (front, back) = canmatrix.formats.xls_common.get_signal(db, sig, motorolaBitFormat)
-                signalRow += front
-                signalRow += ("s" if sig.is_signed else "u")
+                (front, back) = canmatrix.formats.xls_common.get_signal(db, sig, motorola_bit_format)
+                signal_row += front
+                signal_row += ("s" if sig.is_signed else "u")
 
                 col = head_top.__len__()
-                col = writeBuMatrixx(buList, sig, frame, signalRow, col)
-                signalRow += back
+                write_ecu_matrix(ecu_name_list, sig, frame, signal_row, col)
+                signal_row += back
 
                 if sig.min is not None or sig.max is not None:
-                    signalRow += [str("{}..{}".format(sig.min, sig.max))]
+                    signal_row += [str("{}..{}".format(sig.min, sig.max))]
                 else:
-                    signalRow += [""]
+                    signal_row += [""]
 
-                signalRow += additionalFrameInfo
-                for item in additionalSignalCollums:
+                signal_row += additional_frame_info
+                for item in additional_signal_columns:
                     temp = getattr(sig, item, "")
-                    signalRow += [temp]
+                    signal_row += [temp]
 
                 # next row
                 row += 1
-                csvtable.append(signalRow)
+                csv_table.append(signal_row)
                 # set style to normal - without border
         # loop over signals ends here
     # loop over frames ends here
 
     if sys.version_info > (3, 0):
         import io
-        temp = io.TextIOWrapper(thefile, encoding='UTF-8')
+        temp = io.TextIOWrapper(file_object, encoding='UTF-8')
     else:
-        temp = thefile
+        temp = file_object
 
     try:
         writer = csv.writer(temp, delimiter=delimiter)
-        for csv_row in csvtable:
+        for csv_row in csv_table:
             writer.writerow(csv_row.as_list)
         # else:
         #    # just print to stdout
         #    finalTableString = "\n".join(
-        #        [row.toCSV(delimiter) for row in csvtable])
+        #        [row.toCSV(delimiter) for row in csv_table])
         #    print(finalTableString)
     finally:
         if sys.version_info > (3, 0):
