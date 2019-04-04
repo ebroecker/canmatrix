3503cc0c4824442866c6617f28bfa893b0484e54
ebroecker <eduard@gmx.de>
2019-04-04 22:14:06 +0200

    improve j1939 support

--- examples/j1939_decoder.py
+++ examples/j1939_decoder.py
@@ -1,23 +1,29 @@
 import canmatrix.formats
+from pathlib2 import Path
 import attr
 import io
 import textwrap
 
 @attr.s
 class j1939_decoder(object):
-    j1939_db = canmatrix.formats.loadp_flat("j1939.dbc", dbcImportEncoding = "utf8")
+    here = pathlib2.Path(__file__).parent
+
+    j1939_db = canmatrix.formats.loadp_flat(str(here / "j1939.dbc"), dbcImportEncoding = "utf8")
     length = attr.ib(default=0)  # type: int
     count_succesive_frames = attr.ib(default=0)  # type: int
     transfered_pgn = attr.ib(default=0)  # type: int
     _data = attr.ib(init=False, default=bytearray())
 
     def decode(self, arbitration_id, can_data, matrix):
-        if self.j1939_db.frame_by_pgn(arbitration_id.pgn) is not None:
+        frame = matrix.frame_by_pgn(canmatrix.ArbitrationId(id=can_id, extended=True).pgn)
+        if frame is not None:
+            return ("regular", frame.decode(can_data))
+        elif self.j1939_db.frame_by_pgn(arbitration_id.pgn) is not None:
             frame = self.j1939_db.frame_by_pgn(arbitration_id.pgn)
             signals = frame.decode(can_data)
             return ("J1939 known: " + frame.name + " " + frame.comment, signals)
 
-        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn:
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn and can_data[0] == 32:
             # BAM detected
             self.length = (int(can_data[2]) << 8) + int(can_data[1])
             self.count_succesive_frames = int(can_data[3])
@@ -26,7 +32,38 @@ class j1939_decoder(object):
             self._data = bytearray()
             return ("BAM          ", {})
 
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn and can_data[0] == 16:
+            # RTS detected
+            self.length = (int(can_data[2]) << 8) + int(can_data[1])
+            self.count_of_packets = int(can_data[3])
+            self.total_count_of_packet_sent = int(can_data[4])
+            self.transfered_pgn = (int(can_data[7]) << 16) + (int(can_data[6]) << 8) + int(can_data[5])
+
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn and can_data[0] == 17:
+            # CTS detected
+            self.max_packets_at_once = can_data[1]
+            self.sequence_number_to_start = can_data[2]
+            self.transfered_pgn = (int(can_data[7]) << 16) + (int(can_data[6]) << 8) + int(can_data[5])
+
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn and can_data[0] == 19:
+            # ACK detected
+            self.message_size = (int(can_data[2]) << 8) + int(can_data[1])
+            self.count_of_packets = int(can_data[3])
+            self.transfered_pgn = (int(can_data[7]) << 16) + (int(can_data[6]) << 8) + int(can_data[5])
+
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xECFF).pgn and can_data[0] == 255:
+            # Connection Abort
+            self.abort_reason = can_data[1]
+            self.transfered_pgn = (int(can_data[7]) << 16) + (int(can_data[6]) << 8) + int(can_data[5])
+
+        elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xEEFF).pgn:
+            #Address Claimed
+            #arbitration_id.j1939_source
+            #name in can_data[0:8]
+            pass
+
         elif arbitration_id.pgn == canmatrix.ArbitrationId.from_pgn(0xEBFF).pgn:
+            # transfer data
             self.count_succesive_frames -= 1
 
             self._data = self._data + can_data[1:min(8, self.bytes_left + 1)]
