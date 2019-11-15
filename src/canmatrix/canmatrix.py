fe85c7b27bd943300bc6ba6206eb8692741dd7c4
Eduard Bröcker <eduard@gmx.de>
2019-11-15 11:24:49 +0100

    prepare release 0.9.1 (#435)
    * add submodules doc
    * update doc to make start_bit more clear in documentation #424
    * [dbc] fix dbc issues (#431)
    * [dbc] export for empty matrix
    * copy_frame handles default values now (fix #430)
    * [xlsx] export of coplex mutltiplexed frames no longer prohibited (fix #403)

--- src/canmatrix/canmatrix.py
+++ src/canmatrix/canmatrix.py
@@ -128,7 +128,8 @@ class Signal(object):
     Signal has following attributes:
 
     * name
-    * start_bit, size (in Bits)
+    * start_bit (internal start_bit, see get/set_startbit also)
+    * size (in Bits)
     * is_little_endian (1: Intel, 0: Motorola)
     * is_signed (bool)
     * factor, offset, min, max
@@ -987,6 +988,8 @@ class Frame(object):
             self.attributes[attribute] = str(value)
         except UnicodeDecodeError:
             self.attributes[attribute] = value
+        if type(self.attributes[attribute]) == str:
+            self.attributes[attribute] = self.attributes[attribute].strip()
 
     def del_attribute(self, attribute):
         # type: (str) -> typing.Any
