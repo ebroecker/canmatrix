a9c2ef3d7a44cd47b907ce4287898eddd4e9bf1a
ebroecker <eduard@gmx.de>
2020-03-01 20:54:45 +0100

    add number decoding in arxml for different basis possible fix for #454

--- src/canmatrix/utils.py
+++ src/canmatrix/utils.py
@@ -60,3 +60,21 @@ def get_gcd(value1, value2):  # type (int,int) -> (int)
         return math.gcd(value1, value2)
     else:
         return fractions.gcd(value1, value2)
+
+def decode_number(value):  # type(string) -> (int)
+    """
+    Decode string to integer and guess correct base
+    :param value: string input value
+    :return: integer
+    """
+
+    value = value.strip()
+    base = 10
+    if len(value) > 1 and value[1] == 'b':  # bin coded
+        base = 2
+        value = value[2:]
+    if len(value) > 1 and value[1] == 'x':  # hex coded
+        base = 16
+        value = value[2:]
+
+    return int(value, base)
