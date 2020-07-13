ce0bd3a03e6a4f4da77ed32e91e487467a449ebf
ebroecker <eduard@gmx.de>
2020-07-13 09:20:05 +0200

    once again header_id

--- src/canmatrix/formats/arxml.py
+++ src/canmatrix/formats/arxml.py
@@ -1290,12 +1290,12 @@ def get_frame_from_container_ipdu(pdu, target_frame, ea, float_factory):
                                  is_little_endian=True))
             target_frame.add_signal(canmatrix.Signal(start_bit=24, size=8, name="Header_DLC", is_little_endian=True))
         else:  # Motorola
-            header_id_signal= canmatrix.Signal(start_bit=target_frame.size*8-24, size=24, name="Header_ID", multiplex="Multiplexor",
+            header_id_signal= canmatrix.Signal(start_bit=0, size=24, name="Header_ID", multiplex="Multiplexor",
                                  is_little_endian=False)
-            header_id_signal.set_startbit(target_frame.size*8-24)
+    #        header_id_signal.set_startbit(0)
             target_frame.add_signal(header_id_signal)
-            header_dlc_signal = canmatrix.Signal(start_bit=target_frame.size*8-24-8, size=8, name="Header_DLC", is_little_endian=False)
-            header_dlc_signal.set_startbit(target_frame.size*8-32)
+            header_dlc_signal = canmatrix.Signal(start_bit=32, size=8, name="Header_DLC", is_little_endian=False)
+   #         header_dlc_signal.set_startbit(32)
             target_frame.add_signal(header_dlc_signal)
         header_length = 32
 
