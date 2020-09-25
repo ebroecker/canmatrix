ead74452b46a635286f94c158ddd2be69879f3cc
Eduard Bröcker <eduard@gmx.de>
2020-09-25 22:38:36 +0200

    Iss496 (#506)
    * #496 fix ignore encoding switch

--- src/canmatrix/formats/sym.py
+++ src/canmatrix/formats/sym.py
@@ -193,7 +193,7 @@ def dump(db, f, **options):  # type: (canmatrix.CanMatrix, typing.IO, **typing.A
     export canmatrix-object as .sym file (compatible to PEAK-Systems)
     """
     sym_encoding = options.get('symExportEncoding', 'iso-8859-1')
-    ignore_encoding_errors = options.get("ignoreExportEncodingErrors", "")
+    ignore_encoding_errors = options.get("ignoreEncodingErrors", "")
 
     enum_dict = {}
     for enum_name, enum_values in db.value_tables.items():
