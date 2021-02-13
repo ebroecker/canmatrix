fa8c15668a121c3681517bd7fb818e53757fa408
ebroecker <eduard@gmx.de>
2021-02-13 12:43:39 +0100

    Revert "odx zwischnstand"
    This reverts commit f4476de70ab4b51726b240414a23a4e31b498442.

--- aa.py
+++
@@ -1,7 +0,0 @@
-import canmatrix.formats
-
-db = canmatrix.formats.loadp_flat("example.dbc")
-
-for signal in db.frames[0].signals:
-    print("{} Startbit: {}".format(signal.name, signal.get_startbit(bit_numbering=1)))
-
