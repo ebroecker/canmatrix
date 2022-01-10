8ccb76b7b114164f48789960806c31fb07721abd
Darkin <grave.jul@gmail.com>
2022-01-10 12:18:52 +0100

    feat: add support for Python 3.10 (#619)
    * feat: add support for Python 3.10
    * chore: fix appveyor configuration
    * chore: remove useless commands in appveyor configuration
    * chore: Python 3.9 and 3.10 only available for Visual Studio 2019 and up in appveyor

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
 """
 
 from setuptools import find_packages, setup
