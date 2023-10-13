a92d4691ac97227d03b14274ce35d57a70f1908a
Luis Valencia <luis.valencia.medina@gmail.com>
2023-10-13 16:01:52 -0400

    Filter out None results in selector.

--- src/canmatrix/formats/arxml.py
+++ src/canmatrix/formats/arxml.py
@@ -298,7 +298,7 @@ class Earxml:
                                 filtered_results.append(tag)
                     result_list = filtered_results
 
-                result_list = list(set(result_list))
+                result_list = [result for result in set(result_list) if result is not None]
 
             last_found_token = found_token[1] + start_pos
             token = selector[start_pos + found_token[0]:start_pos + found_token[1]]
