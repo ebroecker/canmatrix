537c51b069091d9e2bf298ed9e26d5c755582424
Rowe <ejinn9394@gmail.com>
2024-06-02 21:58:55 +0800

    Move all tests file tests folder in root path

--- src/canmatrix/tests/test_dbf.py
+++
@@ -1,9 +0,0 @@
-import canmatrix.formats.dbc
-import io
-
-
-def test_empty_matrix_export():
-    db = canmatrix.CanMatrix()
-    outdbf = io.BytesIO()
-    canmatrix.formats.dump(db, outdbf, "dbf")
-
