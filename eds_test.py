d1ee16de5c21750d8ab235967cf349df6505fc65
Eduard <eduard@gmx.de>
2025-01-08 08:26:21 +0100

    Revert "support factor and maybe offset #488"
    This reverts commit d7d5458aa02ccd4d5087af27bbee69883921a726.

--- eds_test.py
+++
@@ -1,4 +0,0 @@
-import canmatrix.formats
-db = canmatrix.formats.loadp_flat("/home/edu/Downloads/eds/QG_Ftype_3_axis_8g_v6.2.eds")
-
-print(db)
--- tests/test_ldf.py
+++
@@ -1,5 +0,0 @@
-import canmatrix.formats
-
-cm = canmatrix.formats.loadp_flat("LIN_LDF_EXAMPLE.ldf")
-
-print()
