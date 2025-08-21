07c4d5defa11abcfc71bdaf1da3b834ee3158c7f
Gabriele Omodeo Vanone <mail@gabrieleomodeo.it>
2025-08-21 23:52:19 +0200

    adding new test cases for the Pdu class

--- tests/unit/test_Pdu.py
+++ tests/unit/test_Pdu.py
@@ -32,5 +32,48 @@ import pytest
 from builtins import *
 
 from canmatrix.Pdu import Pdu
+from canmatrix.Signal import Signal
 
-#TODO: add PDU tests
+@pytest.fixture
+def test_odo_signal():
+    return Signal(name="odometer", size=8, factor=1)
+
+@pytest.fixture
+def test_wheelspeed_signal():
+    return Signal(name="wheel speed", size=2, factor=5)
+
+def test_pdu_default_creation():
+    pdu = Pdu()
+    assert pdu.name is ""
+    assert pdu.size is 0
+    assert pdu.id is 0
+    assert pdu.triggering_name is ""
+    assert pdu.pdu_type is ""
+    assert pdu.port_type is ""
+    assert pdu.signals == []            # "is" won't match lists
+    assert pdu.signalGroups == []       # "is" won't match lists
+    assert pdu.cycle_time is 0
+    
+def test_pdu_custom_creation():
+    pdu = Pdu(name="testPdu", size=4, id=0xABCD, triggering_name="trigger", pdu_type="type", port_type="port", signals=[test_odo_signal,test_wheelspeed_signal] , signalGroups=['C'], cycle_time=350)
+    assert pdu.name is "testPdu"
+    assert pdu.size is 4
+    assert pdu.id is 0xABCD
+    assert pdu.triggering_name is "trigger"
+    assert pdu.pdu_type is "type"
+    assert pdu.port_type is "port"
+    assert pdu.signals == [test_odo_signal,test_wheelspeed_signal]            # "is" won't match lists
+    assert pdu.signalGroups == ['C']           # "is" won't match lists
+    assert pdu.cycle_time is 350
+    
+def test_pdu_add_signal():
+    pdu = Pdu()
+    pdu.add_signal(test_odo_signal)
+    assert len(pdu.signals) != 0
+    assert test_odo_signal in pdu.signals
+
+def test_pdu_get_signal_by_name():
+    pdu = Pdu()
+    pdu.add_signal(test_odo_signal)
+    pdu.add_signal(test_wheelspeed_signal)
+    assert pdu.signal_by_name("odometer") == test_odo_signal
