05a29ee93f2882bd329afa4a1d4410fef0d6336d
SabrineBH <sabrine.ben.hmouda@gmail.com>
2025-09-16 10:35:38 +0100

    Support CAN-FD protocol in FIBEX if dbc file define BusType attribute as CAN FD
    Support CAN-FD protocol in FIBEX if dbc file define BusType attribute as CAN FD

--- src/canmatrix/formats/dbc.py
+++ src/canmatrix/formats/dbc.py
@@ -864,6 +864,11 @@ def load(f, **options):  # type: (typing.IO, **typing.Any) -> canmatrix.CanMatri
                         temp.group(2)).add_attribute(
                         temp.group(1),
                         temp.group(3))
+                elif decoded.find('"BusType"') != -1:
+                    regexp = re.compile(r"^BA_ +\"BusType\" +\"(.+?)\" *; *")
+                    temp = regexp.match(decoded)
+                    if temp:
+                        db.add_attribute("BusType", f'"{temp.group(1)}"')
                 else:
                     regexp = re.compile(
                         r"^BA_ +\"([A-Za-z0-9\-_]+)\" +([\"\S\-\.]+) *; *")
--- src/canmatrix/formats/fibex.py
+++ src/canmatrix/formats/fibex.py
@@ -467,7 +467,12 @@ def dump(db, f, **options):
     create_sub_element_fx(cluster, "SPEED", "500")
     create_sub_element_fx(cluster, "IS-HIGH-LOW-BIT-ORDER", "true")
     create_sub_element_fx(cluster, "BIT-COUNTING-POLICY", "MONOTONE")
-    protocol = create_sub_element_fx(cluster, "PROTOCOL", "CAN")
+    attri = db.attributes["BusType"]
+    if attri is not None and attri == "CAN FD":
+        protocol = create_sub_element_fx(cluster, "PROTOCOL", "CAN-FD")
+        create_sub_element_fx(cluster, "CAN-FD-SPEED", "2000000")
+    else:
+        protocol = create_sub_element_fx(cluster, "PROTOCOL", "CAN")
     protocol.attrib['{{{pre}}}type'.format(pre=xsi)] = "can:PROTOCOL-TYPE"
     create_sub_element_fx(cluster, "PROTOCOL-VERSION", "20")
     channel_refs = create_sub_element_fx(cluster, "CHANNEL-REFS")
