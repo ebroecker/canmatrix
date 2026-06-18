28f5c2b1924542609f72509689c440d3a8c95a06
Russ Webber <russ.webber@greenroomrobotics.com>
2026-06-19 01:18:17 +1000

    feat: expose --yamlFrameIdAsHex in canconvert CLI

--- src/canmatrix/cli/convert.py
+++ src/canmatrix/cli/convert.py
@@ -124,6 +124,8 @@ Example --signalNameFromAttrib SysSignalName\nARXML known Attributes: SysSignalN
 @click.option('--jsonExportAll/--no-jsonExportAll', 'jsonExportAll', default=False, help="Export more data to json format")
 @click.option('--jsonMotorolaBitFormat', 'jsonMotorolaBitFormat', default="lsb", help="Json format: startbit of motorola signals\nValid values: msb, lsb, msbreverse\n default lsb")
 @click.option('--jsonNativeTypes/--no-jsonNativeTypes', 'jsonNativeTypes', default=False, help="Uses native json representation for decimals instead of string.")
+# yaml switches
+@click.option('--yamlFrameIdAsHex/--no-yamlFrameIdAsHex', 'yamlFrameIdAsHex', default=False, help="Export frame id as hexadecimal string in yaml format\ndefault: False")
 #eds switches
 @click.option('--edsImportEncoding', 'edsImportEncoding', default="iso-8859-1", help="Import charset of EDS format\ndefault iso-8859-1")
 @click.option('--edsNode', 'eds_node_id', default=1, help="Node-Id for EDS format\ndefault 1")
